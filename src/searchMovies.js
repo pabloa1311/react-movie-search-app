2a4c488454e49c05ef17f94f6180aca0044d547b
pabloa1311 <pabloa1311@gmail.com>
2020-11-12 07:30:40 -0500

    Update searchMovies.js

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -7,16 +7,33 @@ class SearchMovies extends Component {
             query: ""
         }
         this.handleChange = this.handleChange.bind(this)
+        this.searchMovies = this.searchMovies.bind(this)
     }
     
     handleChange(event) {
         const {value, name} = event.target
         this.setState({ [name]:value })
     }
+
+    async searchMovies(event) {
+        event.preventDefault()
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=55267730325e050f50509a8d74572227&language=en-US&query=${this.state.query}&page=1&include_adult=false`
+
+        try {
+            const response = await fetch (url)
+            const data = await response.json()
+
+            console.log(url)
+            console.log(data)
+        }catch(err) {
+            console.error(err)
+        }
+        
+    }
     
     render() {
         return(
-            <form className="form">
+            <form className="form" onSubmit={this.searchMovies}>
                 <label htmlFor="query" className="label">Movie Name</label>
                 <input
                     name="query"
