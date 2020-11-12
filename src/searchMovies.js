4f719d8a641325fb8b111016d0f2feb2ab7f6e39
pabloa1311 <pabloa1311@gmail.com>
2020-11-12 10:31:10 -0500

    Final components

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,57 +1,103 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
+import MovieCard from "./movieCard"
 
-class SearchMovies extends Component {
-    constructor() {
-        super()
-        this.state = {
-            query: ""
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.searchMovies = this.searchMovies.bind(this)
-    }
+function SearchMovies(){
     
-    handleChange(event) {
-        const {value, name} = event.target
-        this.setState({ [name]:value })
-    }
-
-    async searchMovies(event) {
-        event.preventDefault()
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=55267730325e050f50509a8d74572227&language=en-US&query=${this.state.query}&page=1&include_adult=false`
-
-        try {
-            const response = await fetch (url)
-            const data = await response.json()
-
-            console.log(url)
-            console.log(data)
-        }catch(err) {
-            console.error(err)
-        }
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([])
+    
+    const searchMovies = async (e) => {
+        e.preventDefault();
         
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=55267730325e050f50509a8d74572227&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data.results);
+            setMovies(data.results)
+        }catch(err){
+            console.error(err);
+        }
     }
     
-    render() {
-        return(
-            <form className="form" onSubmit={this.searchMovies}>
-                <label htmlFor="query" className="label">Movie Name</label>
-                <input
+    return (
+        <div>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input 
+                    className="input" 
+                    type="text" 
                     name="query"
-                    type="text"
-                    value={this.state.query}
-                    onChange={this.handleChange}
-                    placeholder="Type a Movie"
-                    className="input"                         
+                    value={query}
+                    placeholder="i.e. Jurassic Park"
+                    onChange={(e) => setQuery(e.target.value)}
                 />
-                
-                <button
-                    type="submit"
-                    className="button"
-                >Submit</button>
-                <p>{this.state.query}</p>
+                    
+                <button className="button" type="submit">Search</button>
             </form>
-        )
-    }
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                   <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div> 
+        </div>
+    )
 }
+
+// class SearchMovies extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             query: ""
+//         }
+//         this.handleChange = this.handleChange.bind(this)
+//         this.searchMovies = this.searchMovies.bind(this)
+//     }
+    
+//     handleChange(event) {
+//         const {value, name} = event.target
+//         this.setState({ [name]:value })
+//     }
+
+//     async searchMovies(event) {
+//         event.preventDefault()
+//         const url = `https://api.themoviedb.org/3/search/movie?api_key=55267730325e050f50509a8d74572227&language=en-US&query=${this.state.query}&page=1&include_adult=false`
+
+//         try {
+//             const response = await fetch (url)
+//             const data = await response.json()
+
+//             console.log(url)
+//             console.log(data)
+//         }catch(err) {
+//             console.error(err)
+//         }
+        
+//     }
+    
+//     render() {
+//         return(
+//             <form className="form" onSubmit={this.searchMovies}>
+//                 <label htmlFor="query" className="label">Movie Name</label>
+//                 <input
+//                     name="query"
+//                     type="text"
+//                     value={this.state.query}
+//                     onChange={this.handleChange}
+//                     placeholder="Type a Movie"
+//                     className="input"                         
+//                 />
+                
+//                 <button
+//                     type="submit"
+//                     className="button"
+//                 >Submit</button>
+//                 <p>{this.state.query}</p>
+//             </form>
+//         )
+//     }
+// }
 
 export default SearchMovies
